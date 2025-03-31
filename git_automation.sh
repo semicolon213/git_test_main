dc5a6c17f59d1d50f53c560561b1c0c702b417a5
YJS511 <yongjisoon@gmail.com>
2025-03-31 19:25:47 +0900

    ㄴㅇㄹㅇㄴ

--- git_automation.sh
+++ git_automation.sh
@@ -10,7 +10,7 @@ branches=$(git branch --list | sed 's/^[* ]*//')
 PS3="선택할 브랜치 번호를 입력하세요 : "
 select branch_name in $branches; do
   if [[ -n "$branch_name" ]]; then
-    log+="선택된 브랜치: $branch_name\n"
+    log+="브랜치: $branch_name\n"
     break
   else
     echo "유효하지 않은 선택입니다. 다시 시도하세요."
@@ -22,7 +22,7 @@ PS3="선택할 작업을 입력하세요 : "
 options=("push" "pull")
 select operation in "${options[@]}"; do
   if [[ "$operation" == "push" || "$operation" == "pull" ]]; then
-    log+="선택된 작업: $operation\n"
+    log+="작업: $operation\n"
     break
   else
     echo "유효하지 않은 선택입니다. 다시 시도하세요."
@@ -31,7 +31,7 @@ done
 
 # 커밋 메시지 입력 받기
 read -p "커밋 메시지를 입력하세요 : " commit_message
-log+="커밋 메시지 입력: $commit_message\n"
+log+="커밋 메시지 : $commit_message\n"
 
 # 새 파일 또는 수정된 파일 확인
 new_files=$(git status --porcelain | grep '??' | wc -l)
@@ -40,11 +40,11 @@ modified_files=$(git status --porcelain | grep '^[ M]' | wc -l)
 # 새 파일이 있으면 git add .
 if [[ "$new_files" -gt 0 ]]; then
   add_option="."
-  log+="새 파일이 있어 git add . 을 사용합니다.\n"
+  log+="새로운 파일이 존재하여 git add . 사용\n"
 # 수정된 파일만 있으면 git add -u
 elif [[ "$modified_files" -gt 0 ]]; then
   add_option="-u"
-  log+="수정된 파일만 있어 git add -u 을 사용합니다.\n"
+  log+="수정된 파일만 있어 git add -u 사용\n"
 fi
 
 # 파일 추가
