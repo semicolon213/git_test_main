c018ecafba4ffbbceace670eccaa9f1e5c5533a9
YJS511 <yongjisoon@gmail.com>
2025-03-31 19:11:47 +0900

    test

--- git_automation.sh
+++ git_automation.sh
@@ -7,7 +7,7 @@ log="--------작업로그--------\n"
 branches=$(git branch --list | sed 's/^[* ]*//')
 
 # 브랜치 번호로 목록 보여주기
-PS3="선택할 브랜치 번호를 입력하세요 (1): "
+PS3="선택할 브랜치 번호를 입력하세요 : "
 select branch_name in $branches; do
   if [[ -n "$branch_name" ]]; then
     log+="선택된 브랜치: $branch_name\n"
@@ -18,7 +18,7 @@ select branch_name in $branches; do
 done
 
 # 작업 선택: push 또는 pull
-PS3="선택할 작업을 입력하세요 (1: push, 2: pull): "
+PS3="선택할 작업을 입력하세요 : "
 options=("push" "pull")
 select operation in "${options[@]}"; do
   if [[ "$operation" == "push" || "$operation" == "pull" ]]; then
@@ -30,7 +30,7 @@ select operation in "${options[@]}"; do
 done
 
 # 커밋 메시지 입력 받기
-read -p "커밋 메시지를 입력하세요 (3): " commit_message
+read -p "커밋 메시지를 입력하세요 : " commit_message
 log+="커밋 메시지 입력: $commit_message\n"
 
 # 새 파일 또는 수정된 파일 확인
@@ -49,20 +49,17 @@ fi
 
 # 파일 추가
 git add $add_option
-log+="파일 추가 완료: git add $add_option\n"
 
 # 커밋
 git commit -m "$commit_message"
-log+="커밋 완료: $commit_message\n"
 
 # 선택된 작업 수행 (push 또는 pull)
 if [[ "$operation" == "push" ]]; then
   git push origin "$branch_name"
-  log+="푸시 완료: $branch_name\n"
 elif [[ "$operation" == "pull" ]]; then
   git pull origin "$branch_name"
-  log+="풀 완료: $branch_name\n"
 fi
 
 # 작업 로그 출력
 echo -e "$log"
+echo -e "--------------------"
