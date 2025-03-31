4a6d4973010aaff346e09c4b48ac4339c2a084a9
YJS511 <yongjisoon@gmail.com>
2025-03-31 19:08:17 +0900

    ㅛㅐㅁㅇ니라

--- git_automation.sh
+++ git_automation.sh
@@ -3,6 +3,20 @@
 # 작업 로그 초기화
 log="--------작업로그--------\n"
 
+# 사용 가능한 로컬 브랜치 목록을 가져오기
+branches=$(git branch --list | sed 's/^[* ]*//')
+
+# 브랜치 번호로 목록 보여주기
+PS3="선택할 브랜치 번호를 입력하세요 (1): "
+select branch_name in $branches; do
+  if [[ -n "$branch_name" ]]; then
+    log+="선택된 브랜치: $branch_name\n"
+    break
+  else
+    echo "유효하지 않은 선택입니다. 다시 시도하세요."
+  fi
+done
+
 # 작업 선택: push 또는 pull
 PS3="선택할 작업을 입력하세요 (1: push, 2: pull): "
 options=("push" "pull")
@@ -18,20 +32,6 @@ done
 # 커밋 메시지 입력 받기
 read -p "커밋 메시지를 입력하세요 (3): " commit_message
 log+="커밋 메시지 입력: $commit_message\n"
-
-# 사용 가능한 로컬 브랜치 목록을 가져오기
-branches=$(git branch --list | sed 's/^[* ]*//')
-
-# 브랜치 번호로 목록 보여주기
-PS3="선택할 브랜치 번호를 입력하세요 (1): "
-select branch_name in $branches; do
-  if [[ -n "$branch_name" ]]; then
-    log+="선택된 브랜치: $branch_name\n"
-    break
-  else
-    echo "유효하지 않은 선택입니다. 다시 시도하세요."
-  fi
-done
 
 # 새 파일 또는 수정된 파일 확인
 new_files=$(git status --porcelain | grep '??' | wc -l)
