4e4e38d090f4df2d079df6cb1a7435fb6867b154
YJS511 <yongjisoon@gmail.com>
2025-03-31 17:36:48 +0900

    test yayaya

--- git_automation.sh
+++ git_automation.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
 
+# 커밋 메시지 입력 받기
+read -p "커밋 메시지를 입력하세요: " commit_message
+
+# 브랜치 선택 받기
+read -p "푸시할 브랜치를 입력하세요: " branch_name
+
 # 현재 디렉토리에서 변경된 파일을 git에 추가
 git add .
 
-# 커밋 메시지 입력
-git commit -m "새로운 test"
+# 입력한 커밋 메시지로 커밋
+git commit -m "$commit_message"
 
-# main 브랜치로 푸시 이게 뭐꼬 야야야야야
-git push origin main
+# 선택한 브랜치로 푸시
+git push origin "$branch_name"
